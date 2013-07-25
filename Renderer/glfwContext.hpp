5d0950915e934eda1db8151b746de16a9a529926
Jeff Harrison <jeffrharrison@gmail.com>
2013-07-24 20:19:53 -0500

    Also added the glfw stuff

---
+++ Renderer/glfwContext.hpp
@@ -0,0 +1,52 @@
+#ifndef _GLFWCONTEXT_HPP
+#define _GLFWCONTEXT_HPP
+
+#include <string>
+#include <vector>
+#include <utility>
+#include <map>
+
+#include <GLFW/glfw3.h>
+
+using std::string;
+using std::vector;
+using std::pair;
+using std::make_pair;
+using std::map;
+
+class glfwContext {
+  public:
+    glfwContext();
+    ~glfwContext();
+
+    GLFWwindow* createWindow();
+    void swapBuffers();
+
+    // Getters
+    GLFWwindow* windowHandle();
+    int height();
+    int width();
+
+    // Setters
+    void setHint(int target, int hint);
+    void setHint(const map<int, int> &hintMap);
+    void setHeight(int height);
+    void setWidth(int width);
+    void setTitle(string title);
+
+  private:
+    GLFWwindow* _windowHandle;
+
+    // Window Settings
+    map<int, int> _windowHints;
+    int _width;
+    int _height;
+    string _title;
+    GLFWmonitor* _monitor;
+    glfwContext* _sharedContext;
+
+    // Helper Functions
+    void assignAllHints();
+};
+
+#endif //_CONTEXT_HPP
